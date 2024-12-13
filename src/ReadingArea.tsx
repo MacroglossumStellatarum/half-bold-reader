66aa59c7b1f108cc5f62c81acabd6d6d7a0e7409
MacroglossumStellatarum <smsalmd@protonmail.com>
2024-12-13 22:02:41 +0100

    Streamlined text rendering logic.

--- src/ReadingArea.tsx
+++ src/ReadingArea.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from "jotai";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ScrollSync, ScrollSyncPane } from "react-scroll-sync";
 import {
   alignmentAtom,
@@ -10,6 +10,7 @@ import {
   serifAtom,
   textContrastsAtom,
 } from "./atoms";
+
 import { cn } from "clsx-for-tailwind";
 
 type SegmenterSegment = { segment: string; index: number; input: string; isWordLike: boolean };
@@ -73,19 +74,25 @@ function handleText(
   setJSXtext(applyBoldToHalf(text, unboldedBrightness));
 }
 
+const tutorialText = `Copy your text from elsewhere and paste it here. Or just type.
+\nThe beginning of each word will be bolded. 
+\nMouse over the sides to access various settings as well as the bells and whistles. 
+\nUse your mousewhell to scroll up and down. If you lack one, mouse over the bottom of the reader and use the navigation arrows.
+\nFullscreen for the best experience: 
+F-11 on Windows, COMMAND+SHIFT+F on Mac`
+
 export function ReadingArea() {
-  const [initialClickTriggered, setInitialClickTriggered] = useState(false);
-  const [tutorialFinished, setTutorialFinished] = useState(false);
-  const [textareaText, setTextareaText] = useState("");
-  const [boldedJSXtext, setboldedJSXtext] = useState([] as JSX.Element[]);
+  const initialClickTriggered = useRef(false)
+  const [textareaText, setTextareaText] = useState(tutorialText);
   const currentTextContrast = useAtomValue(currentTextContrastAtom);
   const textContrasts = useAtomValue(textContrastsAtom);
   const serifStatus = useAtomValue(serifAtom);
   const alignmentStatus = useAtomValue(alignmentAtom);
   const currentFontSize = useAtomValue(fontSizeAtom);
-
   const readerWidths = useAtomValue(readerWidthsAtom);
   const currentReaderWidth = useAtomValue(currentReaderWidthAtom);
+
+  const boldedJSXtext = applyBoldToHalf(textareaText, textContrasts[currentTextContrast])
 
   const classesBoth = cn(
     "p-[3rem]",
@@ -106,21 +113,15 @@ export function ReadingArea() {
   );
 
   const classesTextArea = cn(classesBoth, "bg-BG3", "text-[rgba(0,0,0,0)]", "resize-none", "selection:bg-[rgb(127,127,127)]");
-
   const classesJSXText = cn(classesBoth, "pointer-events-none");
 
-  const classesTutorialText = cn(classesBoth);
-
-  useEffect(() => {
-    handleText(textareaText, textContrasts[currentTextContrast], setTextareaText, setboldedJSXtext);
-  }, [currentTextContrast]);
-
-  useEffect(() => {
-    if (initialClickTriggered && !tutorialFinished) {
+  function handleInitialClick(){
+    if (initialClickTriggered.current === false){
       document.getElementById("readingTextArea")?.focus()
-      setTutorialFinished(true);
+      initialClickTriggered.current = true
+      setTextareaText("")
     }
-  }, [initialClickTriggered]);
+  }
 
   return (
     <ScrollSync>
@@ -136,7 +137,8 @@ export function ReadingArea() {
             id="readingTextArea"
             value={textareaText}
             spellCheck="false"
-            onChange={(e: any) => handleText(e.target.value, textContrasts[currentTextContrast], setTextareaText, setboldedJSXtext)}
+            onChange={(e: any) => setTextareaText(e.target.value)}
+            onClick = {handleInitialClick}
           />
         </ScrollSyncPane>
         <ScrollSyncPane>
@@ -147,24 +149,6 @@ export function ReadingArea() {
             {boldedJSXtext}
           </div>
         </ScrollSyncPane>
-
-        {!initialClickTriggered ? (
-          <ScrollSyncPane>
-            <div
-              onClick={() => {
-                setInitialClickTriggered(true);
-              }}
-              className={classesTutorialText}
-            >
-              Copy your text from elsewhere and paste it here. Or just type. {"\n"}
-              {"\n"} The beginning of each word will be bolded. {"\n"}
-              {"\n"} Mouse over the sides to access various settings as well as the bells and whistles. {"\n"}
-              {"\n"} Use your mousewhell to scroll up and down. If you lack one, mouse over the bottom of the reader and use the navigation arrows.{" "}
-              {"\n"}
-              {"\n"} Fullscreen for the best experience: {"\n"}F-11 on Windows, COMMAND+SHIFT+F on Mac
-            </div>
-          </ScrollSyncPane>
-        ) : null}
       </div>
     </ScrollSync>
   );
